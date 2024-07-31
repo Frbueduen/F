4c6036a95238e6c2e88a4793d4493641f7073cc6
Zhi En Noel Ng <s10258408@connect.np.edu.sg>
2024-07-31 20:44:09 +0800

    nigga

--- Main.py
+++ Main.py
@@ -173,7 +173,7 @@ async def search(ctx):
     if rate >= catch:
         await ctx.send(f"{name} was caught!\nCatch roll was {rate} and you needed only {catch} to catch")
             
-            #Add a flee % to decide if user gets another try niggatestcommit
+            #Add a flee % to decide if user gets another try
     else:
         await ctx.send(f"{name} escaped... It rolled a {catch} but you only had {rate}")
 
